9d77463cb43c2b0f4147220ee177376951c97988
Hien <hien.pham@nodo.xyz>
2025-05-14 16:59:07 +0700

    Update line height

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -21,7 +21,7 @@ export default function NodoAIVaults() {
         <div style={{ maxWidth: "1400px" }}>
           {/* Main Header */}
           <header className="text-center mb-16">
-            <h1 className="text-[60px] font-bold">
+            <h1 className="text-[60px] leading-[48px] font-bold">
               <span>NODO </span>
               <span
                 style={{
@@ -36,7 +36,7 @@ export default function NodoAIVaults() {
                 AI Vaults
               </span>
             </h1>
-            <p className="font-body text-">
+            <p className="font-body text-[18px]">
               Maximizing DeFi Yields With Autonomous Risk Management
             </p>
           </header>
