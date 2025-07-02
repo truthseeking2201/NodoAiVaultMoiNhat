02a063ca418f34639429c5d381457d30b75636da
Hien <hien.pham@nodo.xyz>
2025-07-02 13:52:10 +0700

    remove unused http

--- src/apis/vault.ts
+++ src/apis/vault.ts
@@ -1,16 +1,7 @@
 import { DepositVaultConfig } from "@/types/vault-config.types";
-import http from "@/utils/http";
 import httpNodo from "@/utils/httpNodo";
 
-const URLS = {
-  SAMPLE: "/api/v1/poll-user?limit=10&offset=0&order_by=-start_time",
-};
-
 const NODO_URL = import.meta.env.VITE_NODO_APP_URL;
-
-export const getSample = () => {
-  return http.get(URLS.SAMPLE);
-};
 
 export const getLatestWithdrawal = (sender_address) => {
   return httpNodo.get(`/execution/withdrawals/latest/${sender_address}`);
