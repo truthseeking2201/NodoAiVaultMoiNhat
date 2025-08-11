c76a7f3c137e1cde0ff1fb1cad577030a585d633
trantth <trantth@emurgo.africa>
2025-08-11 12:06:39 +0000

    Fix: break vault list

--- src/components/vault/list/user-holding-tooltip.tsx
+++ src/components/vault/list/user-holding-tooltip.tsx
@@ -7,7 +7,12 @@ import {
 const UserHoldingTooltip = ({ children }: { children: React.ReactNode }) => {
   return (
     <Tooltip>
-      <TooltipTrigger type="button">{children}</TooltipTrigger>
+      <TooltipTrigger
+        className="w-full text-left"
+        type="button"
+      >
+        {children}
+      </TooltipTrigger>
       <TooltipContent className="shadow-[0_2px_4px_rgba(255,255,255,0.25)]">
         <div className="text-white/80 font-sans text-xs py-1 max-w-[200px] text-center">
           This is your Current Liquidity and Cumulative Rewards in the vault
